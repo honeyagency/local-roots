a403e13a65f44e16b42d23dec8fb80013a189049
joshre <joshre@me.com>
2016-10-17 22:10:24 -0700

    Mobile Select

--- static/content-list.js
+++ static/content-list.js
@@ -1,16 +1,33 @@
 jQuery(document).ready(function($) {
     $('.content-list > a').on('click touchstart', function(event) {
         event.preventDefault();
-        // console.log($(this));
-        $id = $(this).attr('id');
-        if ($(this).hasClass('current')) {
-        	console.log('fish');
+        $item = $(this);
+        changeContent($item);
+    });
+    $('.contentSelect').on('change', function(event) {
+        event.preventDefault();
+        $item = $(this).val();
+        changeContent($item);
+    });
+
+    function changeContent(value) {
+        $item = value;
+        if ($item.jquery) {
+            $id = $item.attr('id');
+            if ($item.hasClass('current')) {} else {
+                $('.current').removeClass('current');
+                // $('.content-section .current').removeClass('current');
+                $item.toggleClass('current');
+                $content = $('article').find("[data-content='" + $id + "']");
+                $content.toggleClass('current');
+            }
         } else {
+            $id = $item;
             $('.current').removeClass('current');
             // $('.content-section .current').removeClass('current');
-            $(this).toggleClass('current');
+            // $item.toggleClass('current');
             $content = $('article').find("[data-content='" + $id + "']");
             $content.toggleClass('current');
         }
-    });
+    }
 });
